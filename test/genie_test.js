9024d5cf72f237009a2f85d7af49aa0077598cf8
Tessei Yoshida <tessei.yoshida@sindbad-int.com>
2014-02-08 17:39:50 +0900

    Add mocha

---
+++ test/genie_test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when not present', function(){
+      expect([1,2,3].indexOf(4)).to.equal(-1);
+    });
+  });
+});
