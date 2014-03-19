4a8a59dd58f03d408d07ecba1a7cc1df41b7a912
Tessei Yoshida <tessei.yoshida@sindbad-int.com>
2014-03-19 20:26:19 +0900

    update test

--- test/genie_test.js
+++ test/genie_test.js
@@ -3,7 +3,7 @@ var expect = chai.expect;
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when not present', function(){
-      expect([1,2,3].indexOf(4)).to.equal(-1);
+      expect([1,2].indexOf(4)).to.equal(-1);
     });
   });
 });
