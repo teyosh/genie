aac08b4d4373c17801d647e1e4d2265e526a655b
Tessei Yoshida <tessei.yoshida@sindbad-int.com>
2014-03-26 17:23:21 +0900

    Update Add directory module

--- src/FileLoader.js
+++ src/FileLoader.js
@@ -32,27 +32,17 @@ module.exports = (function(){
     }.bind(this));
   };
 
-  FileLoader.prototype.load = function(file, src, suffix){
+  FileLoader.prototype.load = function(file, src, suffix, fileType){
     var suffixReg = new RegExp(suffix+".*");
     var name = file.replace(suffixReg, "");
-    var filepath = "";
-    if(name.match(/[A-Z]/g).length > 1){
-      filepath = this.app.camelCase2Path(name);
-    }
-    src = src || path.join(this.app.appDir, filepath);
+    var filePath = this.filePath(file, src, suffix, fileType);
     var res = {
       name : name,
-      func : require(path.join(src, file))(this.app)
+      func : require(filePath)(this.app)
     };
     return res;
   };
-
-
-  FileLoader.prototype.loadControllers = function(src){
-    this.loadFiles(src, this.suffix.CONTROLLERS);
-  };
-
-  FileLoader.prototype.loadController = function(file, src){
+  FileLoader.prototype.filePath = function(file, src, suffix, fileType){
     if(!/\.js$/.test(file)){
       var filePaths = file.match(/([A-Z][^A-Z]+)/g);
       var fpath = [];
@@ -60,11 +50,34 @@ module.exports = (function(){
         for(var i=0; i<filePaths.length-2; i++){
           fpath.push(filePaths[i]);
         }
-        src = path.join(this.app.appDir,this.src.CONTROLLERS, fpath.join("/"));
+        src = path.join(this.app.appDir, fileType, fpath.join("/"));
+      } else {
+        src = src || path.join(this.app.appDir, fileType);
+        if(!fs.existsSync(this.filePath(file+".js", src, suffix))){
+          for(var j=0; j<filePaths.length-1; j++){
+            fpath.push(filePaths[j]);
+          }
+          src = path.join(this.app.appDir, fileType, fpath.join("/"));
+        }
       }
     }
+    var suffixReg = new RegExp(suffix+".*");
+    var name = file.replace(suffixReg, "");
+    var filepath = "";
+    if(name.match(/[A-Z]/g).length > 1){
+      filepath = this.app.camelCase2Path(name);
+    }
+    src = src || path.join(this.app.appDir, filepath);
+    return path.join(src, file);
+  };
+
+  FileLoader.prototype.loadControllers = function(src){
+    this.loadFiles(src, this.suffix.CONTROLLERS);
+  };
+
+  FileLoader.prototype.loadController = function(file, src){
     src = src || path.join(this.app.appDir,this.src.CONTROLLERS);
-    var loadFn = this.load(file, src, this.suffix.CONTROLLERS);
+    var loadFn = this.load(file, src, this.suffix.CONTROLLERS, this.src.CONTROLLERS);
     if(!this.app.controllers[loadFn.name]){
       this.app.controllers[loadFn.name] = loadFn.func;
     }
@@ -77,7 +90,7 @@ module.exports = (function(){
 
   FileLoader.prototype.loadModel = function(file, src){
     src = src || path.join(this.app.appDir,this.src.MODELS);
-    var loadFn = this.load(file, src, this.suffix.MODELS);
+    var loadFn = this.load(file, src, this.suffix.MODELS, this.src.MODELS);
     if(!this.app.models[loadFn.name]){
       this.app.models[loadFn.name] = loadFn.func;
     }
@@ -90,7 +103,7 @@ module.exports = (function(){
 
   FileLoader.prototype.loadService = function(file, src){
     src = src || path.join(this.app.appDir,this.src.SERVICES);
-    var loadFn = this.load(file, src, this.suffix.SERVICES);
+    var loadFn = this.load(file, src, this.suffix.SERVICES, this.src.SERVICES);
     return this.app.service(loadFn.name, loadFn.func);
   };
 
@@ -100,7 +113,7 @@ module.exports = (function(){
 
   FileLoader.prototype.loadFactory = function(file, src){
     src = src || path.join(this.app.appDir, this.src.FACTORIES);
-    var loadFn = this.load(file, src, this.suffix.FACTORIES);
+    var loadFn = this.load(file, src, this.suffix.FACTORIES, this.src.FACTORIES);
     return this.app.factory(loadFn.name, loadFn.func);
   };
   return FileLoader;
